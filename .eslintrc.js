5990c83e2d2ad35fbc7d6afe7a29e76d939efd9e
doros <luckypalm95@gmail.com>
2023-09-25 12:22:41 +0300

    esling config fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,10 +40,26 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'react/no-array-index-key': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
+        'i18next/no-literal-string':
+            [
+                'error',
+                {
+                    markupOnly: true,
+                    ignoreAttribute: ['data-testid', 'to',
+                    ],
+                },
+            ],
         'max-len': ['error', { ignoreComments: true, code: 120 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
